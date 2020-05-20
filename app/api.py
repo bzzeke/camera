b4caefe567250c90d8fbeaa40950a5d624f8f90b
Ilya Shalnev <zeke@cs-cart.com>
2020-05-20 17:16:04 +0300

    [~] API: "-" is used for any camera/any rule.

--- app/api.py
+++ app/api.py
@@ -21,9 +21,9 @@ class Api:
         if os.path.isfile(filepath):
             db = pickledb.load(filepath, True, sig=False)
             clips = db.lgetall("clips")
-            if camera != "":
+            if camera != "-":
                 clips = list(filter(lambda item: item["camera"] == camera, clips))
-            if rule != "":
+            if rule != "-":
                 clips = list(filter(lambda item: rule in item["objects"], clips))
 
             return sorted(clips, key = lambda item: item["start_time"], reverse=True)
@@ -165,8 +165,11 @@ class ApiHandler(HTTPHandler):
         self.end_headers()
 
         api = Api()
-        camera = args[0] if len(args) >= 1 else ""
-        rule = args[1] if len(args) >= 2 else ""
+        # format: /clips_list
+        # format: /clips_list/-/-/182234644
+        # format: /clips_list/-/person
+        camera = args[0] if len(args) >= 1 else "-"
+        rule = args[1] if len(args) >= 2 else "-"
         date = int(args[2]) if len(args) >= 3 else int(time.time())
 
         status = "ok"
