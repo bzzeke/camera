f506acf12f554ef0962922d321f3cf0df48d4612
Ilya Shalnev <zeke@cs-cart.com>
2020-06-05 15:47:27 +0300

    [~] Bounding box design improvements.

--- app/detector/clip_writer.py
+++ app/detector/clip_writer.py
@@ -3,6 +3,7 @@ import time
 import queue
 import os
 import pickledb
+import numpy as np
 
 from threading import Thread
 
@@ -10,16 +11,20 @@ from api import Api
 from notifier import Notifier
 
 class ClipWriter(Thread):
+
+    COLOR = (153, 255, 51)
     stop = False
     camera = None
     write_queue = None
+    circular_queue = None
     writing = 0
     api = Api()
 
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, camera=None, write_queue=None):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, camera=None, write_queue=None, circular_queue=None):
         super(ClipWriter, self).__init__(group=group, target=target, name=name)
         self.camera = camera
         self.write_queue = write_queue
+        self.circular_queue = circular_queue
 
     def run(self):
 
@@ -33,6 +38,12 @@ class ClipWriter(Thread):
 
                 fourcc = cv2.VideoWriter_fourcc("a", "v", "c", "1")
                 out = cv2.VideoWriter(file_path, fourcc, self.camera["meta"]["fps"], (self.camera["meta"]["width"], self.camera["meta"]["height"]))
+
+                current_size = self.circular_queue.size()
+                i = 0
+                while i < current_size:
+                    i+= 1
+                    out.write(self.circular_queue.get())
 
             if self.writing == 0 and out != None:
                 out.release()
@@ -49,19 +60,33 @@ class ClipWriter(Thread):
 
 
     def make_snapshot(self, objects, frame, timestamp):
+
         origin_im_size = frame.shape[:-1]
+        snapshot_frame = np.array(frame, copy=True)
+        fontface = cv2.FONT_HERSHEY_COMPLEX
+        scale = 0.6
+        thickness = 1
+
         if len(objects) > 0:
             for obj in objects:
                 if obj["xmax"] > origin_im_size[1] or obj["ymax"] > origin_im_size[0] or obj["xmin"] < 0 or obj["ymin"] < 0:
                     continue
-                color = (int(min(obj["class_id"] * 12.5, 255)), min(obj["class_id"] * 7, 255), min(obj["class_id"] * 5, 255))
-                cv2.rectangle(frame, (obj["xmin"], obj["ymin"]), (obj["xmax"], obj["ymax"]), color, 2)
-                cv2.putText(frame, "{}, {}%".format(obj["category"], round(obj["confidence"] * 100, 1)), (obj["xmin"], obj["ymin"] - 7), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
 
+                cv2.rectangle(snapshot_frame, (obj["xmin"], obj["ymin"]), (obj["xmax"], obj["ymax"]), self.COLOR, 2)
+
+                label = "{}, {}%".format(obj["category"], round(obj["confidence"] * 100, 1))
+                (text_width, text_height) = cv2.getTextSize(label, fontface, fontScale=scale, thickness=thickness)[0]
+                text_offset_x = obj["xmin"]
+                text_offset_y = obj["ymin"] - 7
+
+                box_coords = ((obj["xmin"], obj["ymin"]), (obj["xmin"] + text_width + 2, text_offset_y - text_height - 2))
+                cv2.rectangle(snapshot_frame, box_coords[0], box_coords[1], self.COLOR, cv2.FILLED)
+                cv2.putText(snapshot_frame, label, (text_offset_x, text_offset_y), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 1)
 
         file_path = self.api.path(self.camera["name"], timestamp, "jpeg")
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        cv2.imwrite(file_path, frame)
+        cv2.imwrite(file_path, snapshot_frame)
+        del snapshot_frame
 
         notifier = Notifier()
         notifier.notify("Motion detected on camera {}".format(self.camera["name"]), [(file_path, "{}_{}.jpeg".format(self.camera["name"], timestamp))])
