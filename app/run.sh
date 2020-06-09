298309740a3c33c534caef2588168ea4404e655b
Ilya Shalnev <zeke@cs-cart.com>
2020-06-09 22:48:11 +0300

    [~] Automatic model download.

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,19 @@
 
 source /opt/intel/openvino/bin/setupvars.sh
 export OPENCV_LOG_LEVEL=SILENT
+
+MODELS_ROOT="/srv/models"
+FILENAME="${MODEL_PACKAGE##*/}"
+DIRNAME=${FILENAME%.*}
+
+
+if [ ! -d "$MODELS_ROOT/$DIRNAME" ]; then
+    mkdir -p $MODELS_ROOT/$DIRNAME
+    cd $MODELS_ROOT/$DIRNAME
+    curl -SL -o model.tgz -O ${MODEL_PACKAGE}
+    tar -zxf model.tgz
+    rm -f model.tgz
+fi
+
+cd /app
 python3 -u main.py
